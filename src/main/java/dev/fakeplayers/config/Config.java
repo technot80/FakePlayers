832c984d37d251071f309b6236ef3309eee0a47e
technot80 <toringekloumann@gmail.com>
2026-03-01 10:07:28 +0100

    Only save config on upgrade to preserve order
    - Config now only saves when upgrading from old version
    - Existing configs won't get reordered on startup
    - AI section comments preserved in source config.yml

--- src/main/java/dev/fakeplayers/config/Config.java
+++ src/main/java/dev/fakeplayers/config/Config.java
@@ -32,10 +32,9 @@ public class Config {
             plugin.getLogger().info("Upgrading config from version " + configVersion + " to " + CURRENT_CONFIG_VERSION);
             upgradeConfig(configVersion);
             configVersion = CURRENT_CONFIG_VERSION;
+            yaml.set("config-version", CURRENT_CONFIG_VERSION);
+            save();
         }
-        
-        yaml.set("config-version", CURRENT_CONFIG_VERSION);
-        save();
     }
 
     private void upgradeConfig(int fromVersion) {
